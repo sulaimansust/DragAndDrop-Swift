4dae4ad26be44563adca511e735e686ecef99ec1
Sulaiman Khan <sulaiman.sust@gmail.com>
2018-07-18 12:38:43 +0600

    Now drag work fines as expected and also pod is removed now

--- RnDProjectSortImage/ViewController.swift
+++ RnDProjectSortImage/ViewController.swift
@@ -7,10 +7,20 @@
 //
 
 import UIKit
+//import Tab
 
 struct ViewControllerConstants {
     static let kCellIdentifier = "TableViewCellID"
     static let kNibName = "TableViewCell"
+}
+
+struct DraggableCell{
+     var cellSnapshot : UIView? = nil
+     var cellIsAnimating : Bool = false
+     var cellNeedToShow : Bool = false
+}
+struct DraggableCellPath {
+    static var initialIndexPath : IndexPath? = nil
 }
 
 class ViewController: UIViewController {
@@ -18,6 +28,8 @@ class ViewController: UIViewController {
     var cellImageNames : [String] = ["image1","image2","image3","image4","image5","image6"]
     
     @IBOutlet weak var tableView: UITableView!
+    
+    lazy var draggableCell:DraggableCell = DraggableCell.init()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,9 +53,128 @@ class ViewController: UIViewController {
     
     
     @objc fileprivate func onLongPress(gesture: UIGestureRecognizer) -> Void{
+        let longPress = gesture as! UILongPressGestureRecognizer
+        let state = longPress.state
+        
+        let locationInView = longPress.location(in: self.tableView)
+        let indexPath = tableView.indexPathForRow(at: locationInView)
+        
+        
+        switch  state {
+        case .began:
+            
+            if let path = indexPath {
+                DraggableCellPath.initialIndexPath = path
+                
+                let cell = tableView.cellForRow(at: path) as! TableViewCell
+//                draggableCell.cellSnapshot = copy
+                draggableCell.cellSnapshot = copyCellSnapshot(inputView: cell)
+                var center = cell.center
+                draggableCell.cellSnapshot?.center = center
+                draggableCell.cellSnapshot?.alpha = 0.0
+                
+                if let view = draggableCell.cellSnapshot{
+                    tableView.addSubview(view)
+                }
+                
+                UIView.animate(withDuration: 0.25, animations: {
+                    () -> Void in
+                    center.y = locationInView.y
+
+                    self.draggableCell.cellIsAnimating = true
+                    self.draggableCell.cellSnapshot?.center = center
+                    self.draggableCell.cellSnapshot?.transform = CGAffineTransform.init(scaleX: 1.05, y: 1.05)
+                    self.draggableCell.cellSnapshot?.alpha = 0.98
+                    cell.alpha = 0.0
+                    
+                }, completion : {
+                    (finished) -> Void in
+                    
+                    if finished {
+                        self.draggableCell.cellIsAnimating = false
+                        if self.draggableCell.cellNeedToShow {
+                            self.draggableCell.cellNeedToShow = false
+                            UIView.animate(withDuration: 0.25, animations: {
+                                () -> Void in
+                                    cell.alpha = 1.0
+                                })
+                        } else {
+                            cell.isHidden = true
+                        }
+                    }
+                })
+            }
+            
+        case .changed:
+            
+            if let cell = self.draggableCell.cellSnapshot {
+                var center = cell.center
+                center.y = locationInView.y
+                cell.center = center
+            }
+            
+            if let path = indexPath, path != DraggableCellPath.initialIndexPath {
+                if let initialPath = DraggableCellPath.initialIndexPath {
+                    cellImageNames.insert(cellImageNames.remove(at: initialPath.row), at: path.row)
+                    
+                    self.tableView.moveRow(at: initialPath, to: path)
+                    DraggableCellPath.initialIndexPath = path
+                }
+            }
+            
+        default:
+            if let initialIndexPath = DraggableCellPath.initialIndexPath {
+                let cell = tableView.cellForRow(at: initialIndexPath) as! TableViewCell
+                
+                if self.draggableCell.cellIsAnimating {
+                    self.draggableCell.cellNeedToShow = true
+                } else {
+                    cell.isHidden = false
+                    cell.alpha = 0.0
+                }
+                
+                UIView.animate(withDuration: 0.25, animations: {
+                    () -> Void in
+                    
+                    self.draggableCell.cellSnapshot?.center = cell.center
+                    self.draggableCell.cellSnapshot?.transform = CGAffineTransform.identity
+                    self.draggableCell.cellSnapshot?.alpha = 0.0
+                    cell.alpha = 1.0
+                    
+                }, completion : {
+                    (finished) -> Void in
+                    
+                    if finished {
+                        DraggableCellPath.initialIndexPath = nil
+                        self.draggableCell.cellSnapshot?.removeFromSuperview()
+                        self.draggableCell.cellSnapshot = nil
+                    }
+                    
+                })
+                
+                
+            }
+        }
         
     }
     
+    fileprivate func copyCellSnapshot(inputView: UIView ) -> UIView {
+        
+        UIGraphicsBeginImageContextWithOptions(inputView.bounds.size, false, 0.0)
+        inputView.layer.render(in: UIGraphicsGetCurrentContext()!)
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        let cellSnapshot : UIView = UIImageView.init(image: image)
+        cellSnapshot.layer.masksToBounds = true
+        cellSnapshot.layer.cornerRadius = 0.0
+        cellSnapshot.layer.shadowOffset = CGSize.init(width: -5, height: 0)
+        cellSnapshot.layer.shadowRadius = 5
+        cellSnapshot.layer.shadowOpacity = 0.4
+        return cellSnapshot
+        
+    }
 
 }
 
@@ -63,15 +194,17 @@ extension ViewController : UITableViewDataSource {
             return cell
             
         }
-        
-    
-        
         return UITableViewCell.init()
         
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 125
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.tableView.deselectRow(at: indexPath, animated: true
+        )
     }
     
     
